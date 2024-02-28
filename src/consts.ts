04774511fc7ee4bcc11f8aac13e62e5678285608
Kevin <106118038+Spinelk@users.noreply.github.com>
2024-02-28 19:25:51 -0300

    Agregados enlaces de Facebook e Instagram en el Navbar y el Footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
 export const TITULO_PAGINA = 'Fundación Casa de Jesús';
 export const DESCRIPCION_PAGINA = 'Bienvenidos a la Fundación Casa de Jesús';
+export const ENLACE_FACEBOOK = 'https://www.facebook.com/fundacioneiglesiacasadejesus/';
+export const ENLACE_INSTAGRAM = 'https://www.instagram.com/casadejesusoficial/';
