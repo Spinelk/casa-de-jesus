08b688b20c695be96bf5f7d3b30ec2d34671e13e
Kevin <106118038+Spinelk@users.noreply.github.com>
2024-03-11 00:36:28 -0300

    Añadir constante de correo de contacto

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,10 @@
-export const TITULO_PAGINA = 'Fundación Casa de Jesús';
-export const DESCRIPCION_PAGINA = 'Bienvenidos a la Fundación Casa de Jesús';
-export const ENLACE_FACEBOOK = 'https://www.facebook.com/fundacioneiglesiacasadejesus/';
-export const ENLACE_INSTAGRAM = 'https://www.instagram.com/casadejesusoficial/';
+export const TITULO_PAGINA = "Fundación Casa de Jesús";
+export const DESCRIPCION_PAGINA = "Bienvenidos a la Fundación Casa de Jesús";
+
+export const ENLACE_FACEBOOK = "https://www.facebook.com/fundacioneiglesiacasadejesus/";
+export const ENLACE_INSTAGRAM = "https://www.instagram.com/casadejesusoficial/";
+
+export const TELEFONO_CONTACTO = "(2) 2683 2719";
+export const WHATSAPP_CONTACTO = "+569 5678 4830";
+
+export const CORREO_CONTACTO = "fundacioncasadejesus@gmail.com";
